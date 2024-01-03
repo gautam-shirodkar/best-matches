412228b3157dfc59124e0608d1bf63d78440bf93
Gautam <gautam.shirodkar92@gmail.com>
2024-01-03 19:37:13 +0530

    add more routes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import { QueryClientProvider } from "react-query";
 import "./App.css";
 import Matches from "./components/Matches/Matches";
 import { queryClient } from "./services/http";
+import NewMatch from "./components/Matches/NewMatch";
 
-const router = createBrowserRouter([{ path: "/", element: <Matches /> }]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Matches />,
+    children: [
+      {
+        path: "matches/new",
+        element: <NewMatch />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
